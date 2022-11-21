38c58c3ec84811d3876696b0f7df5033504d8c6a
AkshayV30 <akshayatwork.v30@gmail.com>
2022-11-21 12:51:44 +0530

    terra-update-03

--- main.tf
+++ main.tf
@@ -50,14 +50,6 @@ terraform {
     }
 }
 
-# Configuring provider
-# provider "aws" {
-#     region = "ap-south-1"
-#     access_key = "my-access-key"
-#     secret_key = "my-secret-key"
-#     # key_name = "app-ssh-key"
-# }
-
 # Creating a VPC
 resource "aws_vpc" "test-vpc" {
     cidr_block = "10.0.0.0/16"
